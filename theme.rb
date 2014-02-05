2cf14e3edc166ceb3c54246413a4837de6ee89a6
Masafumi Yokoyama <myokoym@gmail.com>
2014-02-05 23:52:43 +0900

    Decorate slides with images

---
+++ theme.rb
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+@font_family = find_font_family("IPA Pゴシック") ||
+               find_font_family("IPAPGothic") ||
+               @font_family
+include_theme("nyankosakana")
