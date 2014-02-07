c67af81115d8ed65edbf5aba28206b9e7093d6a7
Masafumi Yokoyama <myokoym@gmail.com>
2014-02-08 03:16:05 +0900

    Update

--- theme.rb
+++ theme.rb
@@ -2,4 +2,6 @@
 @font_family = find_font_family("IPA Pゴシック") ||
                find_font_family("IPAPGothic") ||
                @font_family
+@monospace_font_family = find_font_family("Ubuntu Mono") ||
+	                 @monospace_font_family
 include_theme("nyankosakana")
