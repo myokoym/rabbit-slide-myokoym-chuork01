d853435883cd3c56eb866fc623c40732d95c86d1
Masafumi Yokoyama <myokoym@gmail.com>
2014-02-11 11:44:58 +0900

    Use spaces for indent

--- theme.rb
+++ theme.rb
@@ -3,5 +3,5 @@
                find_font_family("IPAPGothic") ||
                @font_family
 @monospace_font_family = find_font_family("Ubuntu Mono") ||
-	                 @monospace_font_family
+                         @monospace_font_family
 include_theme("nyankosakana")
